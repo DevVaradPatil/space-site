8cb5ed94d81b639f589a8e81c99658d112088dca
DevVaradPatil <varadapatil123@gmail.com>
2024-12-07 21:25:51 +0530

    Adjust loading timeout and enhance Navbar styles for better responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
-    }, 3500);
+    }, 3300);
   }, []);
 
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,11 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className='w-full py-4 flex absolute top-0 left-0 justify-center items-center px-4 md:px-8'>
-            <div className='absolute left-5 top-4 w-10'>
+        <div className='w-full py-4 flex fixed bg-transparent backdrop-blur-md z-50 top-0 left-0 justify-between md:justify-center items-center px-4 md:px-8'>
+            <div className='hidden md:flex absolute left-5 top-4 w-10'>
+                <img src={logo} alt="Stellar Explorers" />
+            </div>
+            <div className='flex md:hidden w-10'>
                 <img src={logo} alt="Stellar Explorers" />
             </div>
             <div className='hidden md:flex space-x-8 text-xl uppercase tracking-wider'>
@@ -14,7 +17,7 @@ const Navbar = () => {
                 <a href='#team' className='text-neutral-100 font-orbitron hover:text-secondary transition duration-200 transform hover:scale-110'>Team</a>
                 <a href='#contact' className='text-neutral-100 font-orbitron hover:text-secondary transition duration-200 transform hover:scale-110'>Contact</a>
             </div>
-            <div className='md:hidden absolute right-5 top-5'>
+            <div className='md:hidden'>
                 <button onClick={() => setIsOpen(!isOpen)} className='text-neutral-100 focus:outline-none'>
                     <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
                         <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16m-7 6h7'}></path>
